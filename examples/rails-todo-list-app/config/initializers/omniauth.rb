c0b042320600e35bcea437f86a51bc73535e1030
Stefan Eilers <stefan.eilers@intelligentmobiles.com>
2020-12-23 15:35:33 +0100

    Runtime switching between instances of id providers of the same type. See: https://github.com/omniauth/omniauth/wiki/Setup-Phase

--- examples/rails-todo-list-app/config/initializers/omniauth.rb
+++ examples/rails-todo-list-app/config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
+SETUP_PROC = lambda do |env|
+    request = Rack::Request.new(env)
+    #user = User.find_by_subdomain(request.subdomain)
+    # TODO: find the organization by an id provided with the login path. This will give us the credentials..
+    env['omniauth.strategy'].options[:client_id] = ENV['CLIENT_ID']         # if using omniauth-oauth2
+    env['omniauth.strategy'].options[:tenant] = ENV['TENANT']   # if using omniauth-oauth2
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :azure_activedirectory, ENV['CLIENT_ID'], ENV['TENANT']
+    provider :azure_activedirectory, :setup => SETUP_PROC
 end
